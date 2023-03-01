d711d6866dde20e5bdad64b8d7d34c7e62c2ebfd
Juho Valtonen <juho@valtoset.net>
2023-03-01 14:22:37 +0200

    Move things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { Component, Item, Page } from "@/pages/api/page";
+import { Page } from "@/pages/api/page";
 import { Suspense } from "react";
+import { Renderer } from "../components/Renderer";
 
 export default async function Home() {
   const page = await fetch("http://localhost:3000/api/page", {
@@ -15,92 +16,6 @@ export default async function Home() {
           <Renderer componentOrItem={component} />
         </Suspense>
       ))}
-    </div>
-  );
-}
-
-async function Renderer({
-  componentOrItem,
-}: {
-  componentOrItem: Component | Item;
-}) {
-  /**
-   * Containers
-   */
-  if ("items" in componentOrItem) {
-    const component = componentOrItem;
-
-    let items = component.items;
-    const query = component.query;
-
-    if (items.length === 0 && query) {
-      const response = await fetch(`http://localhost:3000/api/component`, {
-        cache: "no-cache",
-      });
-      const componentData = (await response.json()) as Component;
-      items = componentData.items;
-    }
-
-    const children = items.map((item) => (
-      // @ts-expect-error Server Component
-      <Renderer key={item.id} componentOrItem={item} />
-    ));
-
-    switch (component.type) {
-      case "container-a":
-        return <ContainerA>{children}</ContainerA>;
-      case "container-b":
-        return <ContainerB>{children}</ContainerB>;
-      default:
-        return <div>Unknown component</div>;
-    }
-  } else {
-    /**
-     * Items
-     */
-    const item = componentOrItem;
-
-    switch (item.type) {
-      case "red":
-        return <ItemRed item={item} />;
-      case "blue":
-        return <ItemBlue item={item} />;
-      default:
-        return <div>Unknown item</div>;
-    }
-  }
-}
-
-function ContainerA({ children }: { children: React.ReactNode }) {
-  return (
-    <fieldset>
-      <legend>Container A</legend>
-      <div>{children}</div>
-    </fieldset>
-  );
-}
-
-function ContainerB({ children }: { children: React.ReactNode }) {
-  return (
-    <fieldset>
-      <legend>Container B</legend>
-      <div>{children}</div>
-    </fieldset>
-  );
-}
-
-function ItemBlue({ item }: { item: Item }) {
-  return (
-    <div className="item blue">
-      <div>{item.title}</div>
-    </div>
-  );
-}
-
-function ItemRed({ item }: { item: Item }) {
-  return (
-    <div className="item red">
-      <div>{item.title}</div>
     </div>
   );
 }
---
+++ src/components/Renderer.tsx
@@ -0,0 +1,87 @@
+import { Component, Item } from "@/pages/api/page";
+
+export async function Renderer({
+  componentOrItem,
+}: {
+  componentOrItem: Component | Item;
+}) {
+  /**
+   * Containers
+   */
+  if ("items" in componentOrItem) {
+    const component = componentOrItem;
+
+    let items = component.items;
+    const query = component.query;
+
+    if (items.length === 0 && query) {
+      const response = await fetch(`http://localhost:3000/api/component`, {
+        cache: "no-cache",
+      });
+      const componentData = (await response.json()) as Component;
+      items = componentData.items;
+    }
+
+    const children = items.map((item) => (
+      // @ts-expect-error Server Component
+      <Renderer key={item.id} componentOrItem={item} />
+    ));
+
+    switch (component.type) {
+      case "container-a":
+        return <ContainerA>{children}</ContainerA>;
+      case "container-b":
+        return <ContainerB>{children}</ContainerB>;
+      default:
+        return <div>Unknown component</div>;
+    }
+  } else {
+    /**
+     * Items
+     */
+    const item = componentOrItem;
+
+    switch (item.type) {
+      case "red":
+        return <ItemRed item={item} />;
+      case "blue":
+        return <ItemBlue item={item} />;
+      default:
+        return <div>Unknown item</div>;
+    }
+  }
+}
+
+function ContainerA({ children }: { children: React.ReactNode }) {
+  return (
+    <fieldset>
+      <legend>Container A</legend>
+      <div>{children}</div>
+    </fieldset>
+  );
+}
+
+function ContainerB({ children }: { children: React.ReactNode }) {
+  return (
+    <fieldset>
+      <legend>Container B</legend>
+      <div>{children}</div>
+    </fieldset>
+  );
+}
+
+function ItemBlue({ item }: { item: Item }) {
+  return (
+    <div className="item blue">
+      <div>{item.title}</div>
+    </div>
+  );
+}
+
+function ItemRed({ item }: { item: Item }) {
+  return (
+    <div className="item red">
+      <div>{item.title}</div>
+    </div>
+  );
+}
